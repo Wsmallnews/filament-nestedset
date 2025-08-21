3e5575e4fa8c8397e87a1a32856a52a5c5598268
smallnews <1371606921@qq.com>
2025-08-21 23:29:03 +0800

    extend CodeWithDennis SelectTree, support ParentSelect

--- src/Pages/NestedsetPage.php
+++ src/Pages/NestedsetPage.php
@@ -2,7 +2,6 @@
 
 namespace Wsmallnews\FilamentNestedset\Pages;
 
-use CodeWithDennis\FilamentSelectTree\SelectTree;
 use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
@@ -17,6 +16,7 @@ use Filament\Pages\Page;
 use Filament\Resources\Concerns\HasTabs;
 use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\IconSize;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Kalnoy\Nestedset\NestedSet;
@@ -26,6 +26,7 @@ use Livewire\Attributes\Url;
 use Livewire\Features\SupportEvents\Event;
 use Throwable;
 use Wsmallnews\FilamentNestedset\Exceptions\NestedsetException;
+use Wsmallnews\FilamentNestedset\Forms\Fields\KalnoyNestedsetSelectTree;
 
 use function Filament\Support\get_model_label;
 
@@ -125,21 +126,19 @@ abstract class NestedsetPage extends Page
                     ...$model->getAttributes(),          // 这里填充 scoped 设置的数据
                 ];
             })
-            ->form(fn (array $arguments): array => method_exists($this, 'createSchema') ? $this->createSchema($arguments) : $this->schema($arguments))
+            ->form(function (array $arguments) use ($type) {
+                $schema = method_exists($this, 'createSchema') ? $this->createSchema($arguments) : $this->schema($arguments);
 
-            // ->form(function (array $arguments) use ($type) {
-            //     $schema = method_exists($this, 'createSchema') ? $this->createSchema($arguments) : $this->schema($arguments);
+                if ($type == 'create' && $this->level >= 2 && $this->hasFormParentSelect()) {       // 创建，并且 nesetdset level 至少两级才可以选择上级
+                    $parentSelect = Arr::wrap($this->getParentSelect());
 
-            //     if ($type == 'create' && $this->hasFormParentSelect()) {
-            //         $parentSelect = Arr::wrap($this->getParentSelect());
+                    $schema = array_merge([
+                        ...$parentSelect
+                    ], $schema);
+                }
 
-            //         $schema = array_merge([
-            //             ...$parentSelect
-            //         ], $schema);
-            //     }
-
-            //     return $schema;
-            // })
+                return $schema;
+            })
             ->using(function (array $data, array $arguments): Model {
                 // 优先使用表单中的 parent_id
                 $parentId = $data['parent_id'] ?? ($arguments['parentId'] ?? 0);
@@ -305,25 +304,29 @@ abstract class NestedsetPage extends Page
         return ! (config('sn-filament-nestedset.allow_delete_root') === false && $record->children->isNotEmpty() && $record->isRoot());
     }
 
-    // protected function hasFormParentSelect(): bool
-    // {
-    //     $childrenAddMethod = config('sn-filament-nestedset.children_add_method') ?? 'both';
+    protected function hasFormParentSelect(): bool
+    {
+        $childrenAddMethod = config('sn-filament-nestedset.children_add_method') ?? 'both';
 
-    //     return in_array($childrenAddMethod, ['both', 'form']);
-    // }
+        return in_array($childrenAddMethod, ['both', 'form']);
+    }
 
-    // protected function getParentSelect(): array | Field
-    // {
-    //     return SelectTree::make('parent_id')->label('父节点')
-    //         ->relationship(relationship: 'parent', titleAttribute: 'name', parentAttribute: NestedSet::PARENT_ID)
-    //         ->searchable()
-    //         ->enableBranchNode()     // 可以选择非根节点
-    //         ->withCount()
-    //         ->required()
-    //         ->placeholder('请选择父节点')
-    //         ->emptyLabel('未搜索到父节点')
-    //         ->treeKey('NestedParentId');
-    // }
+    protected function getParentSelect(): array | Field
+    {
+        return KalnoyNestedsetSelectTree::make('parent_id')->label('父节点')
+            ->level($this->level - 1)      // 能让用户选择的层级，需要 -1
+            ->relationship(relationship: 'parent', titleAttribute: 'name', parentAttribute: NestedSet::PARENT_ID)
+            ->searchable()
+            ->buildQuery(function () {
+                return $this->getQuery();
+            })
+            ->enableBranchNode()     // 可以选择非根节点
+            ->withCount()
+            ->required()
+            ->placeholder('请选择父节点')
+            ->emptyLabel('未搜索到父节点')
+            ->treeKey('NestedParentId');
+    }
 
     public function getRecordTitleAttribute(): string
     {
