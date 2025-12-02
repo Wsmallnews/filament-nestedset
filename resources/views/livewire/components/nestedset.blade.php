18d16ff8371d10727a70a5809090dd5bbe8ff19a
smallnews@qq.com <1371606921@qq.com>
2025-12-02 18:53:45 +0800

    添加 nestedset 组件

---
+++ resources/views/livewire/components/nestedset.blade.php
@@ -0,0 +1,34 @@
+@php
+    $level = $this->getLevel();
+    $nestedset = $this->getCategories();
+@endphp
+
+<ul
+    @class([
+        'w-full flex flex-col',
+        'bg-primary-500 divide-y divide-primary-400' => $style === 'vivid',
+    ])
+    role="menu"
+>
+    @forelse($categories as $treeKey => $record)
+        <x-dynamic-component 
+            @class([
+                'w-full',
+            ]) 
+            :component="$this->getItemView()" 
+            key="categories-component-{{ $record->getKey() }}" 
+            :record="$record" 
+            :level="$level" 
+            :first="$loop->first" 
+            :last="$loop->last" 
+            :style="$style" 
+            :current-level="1" 
+        />
+    @empty
+        <li 
+            class="w-full px-3 py-2 text-center"
+        >
+            {{ $this->getEmptyLabel() ?: '未获取到分类数据' }}
+        </li>
+    @endforelse
+</ul>
---
+++ src/Livewire/Components/Nestedset.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Wsmallnews\FilamentNestedset\Livewire\Components;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\HtmlString;
+use Livewire\Component;
+
+class Nestedset extends Component
+{
+    public ?string $view = 'sn-filament-nestedset::livewire.components.categories';
+
+    public ?string $itemView = 'sn-filament-nestedset::category';
+
+    public ?string $style = 'simple';        // vivid=鲜明的, simple=简单的
+
+    public string $recordTitleAttribute = 'name';
+
+    protected static ?string $showLevel = null;
+
+    protected static ?string $emptyLabel = '';
+    
+    protected static ?string $model = null;
+
+    public function getShowLevel(): ?int
+    {
+        return static::$showLevel;
+    }
+
+    public function getEmptyLabel(): ?string
+    {
+        return static::$emptyLabel;
+    }
+
+    public static function getModel()
+    {
+        return static::$model;
+    }
+
+    public static function getRecordTitleAttribute(): ?string
+    {
+        return static::$recordTitleAttribute;
+    }
+
+    public function getRecordLabel(Model $record): HtmlString | string
+    {
+        return $record->{static::getRecordTitleAttribute()} ?? ' ';
+    }
+
+    public function getItemView(): string
+    {
+        return $this->itemView;
+    }
+
+
+    protected function getQuery()
+    {
+        $model = static::getModel();
+
+        $scopes = [];
+        // 自定义 scope
+        if (method_exists($this, 'nestedScoped')) {
+            $scopes = array_merge($scopes, $this->nestedScoped());
+        }
+
+        if ($scopes) {
+            $query = $model::scoped($scopes);
+        } else {
+            $query = (new $model)->newScopedQuery();
+        }
+
+        // 自定义条件
+        if (method_exists($this, 'getEloquentQuery')) {
+            $query = $this->getEloquentQuery($query);
+        }
+
+        $query = $query->defaultOrder();
+
+        return $query;
+    }
+
+    public function getNestedset()
+    {
+        $nestedset = $this->getQuery()->withDepth()->get();
+
+        if (static::getShowLevel() !== null) {
+            $nestedset = $nestedset->filter(function ($record) {
+                return $record->depth <= static::getShowLevel();
+            });
+        }
+
+        return $nestedset->toTree();
+    }
+
+
+    public function render()
+    {
+        return view($this->view);
+    }
+}
